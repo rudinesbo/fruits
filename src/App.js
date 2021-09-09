48188f2f457d3715fc2cc5f7911fb5566e7f25f0
rudinesbo <nesborudi@gmail.com>
2021-09-09 12:28:16 +0200

    remove overflow and improve width-detection

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { useMediaQuery } from "react-responsive";
-
 import Home from "./components/Home";
 import HomeMobile from "./components/HomeMobile";
 
 import Fruits from "./components/Fruits";
 
 function App() {
-  const isMobileDevice = useMediaQuery({
-    query: "(max-device-width: 1023px)",
-  });
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  if (isMobileDevice) {
+  useEffect(() => {
+    function handleResize() {
+      setWindowWidth(window.innerWidth);
+    }
+
+    window.addEventListener("resize", handleResize);
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  if (windowWidth < 1024) {
     return (
       <Router>
         <div className="mobile">
